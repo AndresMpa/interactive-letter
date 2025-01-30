8dd07c2878db4b0a3658a61f4c8e1bd2016eb304
Andres Prieto <andresprieto2703@gmail.com>
2025-01-30 13:57:40 -0500

    UPDATE: Icon
    Updating assets for icon on Storybook doc

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -4,6 +4,6 @@ export default create({
   base: 'light',
   brandTitle: 'A letter',
   brandUrl: 'https://github.com/AndresMpa/interactive-letter',
-  brandImage: './assets/icon.png',
+  brandImage: 'https://raw.githubusercontent.com/AndresMpa/interactive-letter/refs/heads/main/.storybook/assets/icon.png',
   brandTarget: '_self',
 });
