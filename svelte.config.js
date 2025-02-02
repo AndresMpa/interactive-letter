0987392dda7b043a1495ba1d19b05b4c8cc38d54
Andres Prieto <andresprieto2703@gmail.com>
2025-02-02 00:02:50 -0500

    UPDATE: Svelte config
    Tracking some features from vite on svelte to open components

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from "mdsvex";
+import { mdsvex } from 'mdsvex';
 
 const mdsvexConfig = {
 	extensions: ['.svelte.md', '.md', '.svx']
@@ -18,7 +18,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter()
+	},
+	vitePlugin: {
+		// Allows you to hold ctrl+shift and click on an item in the browser and it then
+		// opens that components location in VSCode
+		inspector: {
+			holdMode: true
+		}
 	}
 };
 
